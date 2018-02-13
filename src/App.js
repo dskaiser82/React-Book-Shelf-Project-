3c203048413b0390c3c258853efdf83fa964dd7c
Dan Kaiser <dskaiser82@gmail.com>
2018-02-13 12:17:07 -0800

    we got the books, bro

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ class BooksApp extends React.Component {
   willRead: []
 }
 
+componentDidMount(){
+  BooksAPI.getAll().then((allBooks) => {
+    this.setState({ allBooks })
+    console.log(allBooks)
+ })
+}
 
   render() {
     return (
