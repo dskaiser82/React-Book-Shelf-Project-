eca35767a06dfec12c01e1e37fcc4cba0553dc09
Dan Kaiser <dskaiser82@gmail.com>
2018-02-17 22:04:38 -0800

    well I found the search API

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ class BooksApp extends React.Component {
   state = {
   allBooks: [],
   targetShelf: {},  //prob dont need this
-  targetId: {}   //prob dont need this
+  targetId: {},   //prob dont need this
+  search:[]
 }
 
   componentDidMount(){
     BooksAPI.getAll().then((allBooks) => {
       this.setState({ allBooks })
+      console.log(allBooks)
+   })
+   BooksAPI.search("design").then((search) => {
+     this.setState({ search })
+     console.log(search)
    })
   }
 
@@ -22,6 +28,7 @@ class BooksApp extends React.Component {
     this.setState((previousState) => {
     const bookie = previousState.allBooks.filter(book => book.id === targetId);
     bookie[0].shelf = targetShelf;
+
     });
 
     //Send Updated Book to API
