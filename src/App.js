342672cd61966e9b5687c451b56a57fb28d8abf8
Dan Kaiser <dskaiser82@gmail.com>
2018-02-18 21:07:22 -0800

    got the search books minus image

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ class BooksApp extends React.Component {
   componentDidMount(){
     BooksAPI.getAll().then((allBooks) => {
       this.setState({ allBooks })
-      console.log(allBooks)
    })
    //So These Search Dont Have book.shelf.  But I may cheat anf use Book.rating and put the string there
    BooksAPI.search(this.state.searchTerm).then((searchBooks) => {
@@ -66,6 +65,8 @@ class BooksApp extends React.Component {
       )}/>
       <Route path="/search" render={({ history }) => (
           <SearchBook
+            searchBooks={this.state.searchBooks}
+            updateTarget={this.updateTarget}
             //DK << THE HISTORY STUFF SO IT CAN SEND YOU BACK TO HOME PAGE ON AN ACTION SEE MY NOTES
             // onCreateContact={(contact) => {
             //   this.CreateContact(contact)
--- src/SearchBook.js
+++ src/SearchBook.js
@@ -5,6 +5,8 @@ import { Link} from 'react-router-dom';
 class SearchBook extends Component{
 
   render(){
+    const { searchBooks, updateTarget} = this.props
+
     return(
     <div>
       <div className="search-books">
@@ -15,7 +17,37 @@ class SearchBook extends Component{
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+
+            {searchBooks.filter(book => book.shelf !== "currentlyReading")
+              .map((book) =>(
+                <li key={book.id}>
+                  <div className="book">
+                    <div className="book-top">
+                      {/* <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div> */}
+                      <div className="book-shelf-changer">
+
+                        <select id={book.id}
+                          onChange={(event) => updateTarget(event.target.value, event.target.id)}
+                          >
+
+                          <option value="none" defaultValue>Move to...</option>
+                          <option value="currentlyReading">Currently Reading</option>
+                          <option value="wantToRead">Want to Read</option>
+                          <option value="read">Read</option>
+                          <option value="none">None</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="book-title">{book.title}</div>
+                    <div className="book-authors">{book.authors}</div>
+                  </div>
+                </li>
+              ))
+            }
+
+
+          </ol>
         </div>
       </div>
       {/* BACK HOME*/}
