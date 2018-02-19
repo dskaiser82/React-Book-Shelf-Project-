5f1467aa5403dc10da76667e268af53b2ad10734
Dan Kaiser <dskaiser82@gmail.com>
2018-02-19 13:04:20 -0800

    cleaned up negative view

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -24,7 +24,8 @@ class SearchBook extends Component{
           </Link>
           <ol className="books-grid">
 
-            {searchBooks.filter(book => book.shelf !== "currentlyReading")
+            {searchBooks.filter(book => book.shelf !== "currentlyReading"
+            && book.shelf !== "read" && book.shelf !== "wantToRead")
               .map((book) =>(
                 <li key={book.id}>
                   <div className="book">
