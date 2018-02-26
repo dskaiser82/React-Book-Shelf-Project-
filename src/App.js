4b72d280fc5b85f746cd2a5aaf6e48dd8f6d8020
Dan Kaiser <dskaiser82@gmail.com>
2018-02-25 22:58:34 -0800

    have a mtach but u need to have an extra space

--- src/App.js
+++ src/App.js
@@ -56,10 +56,19 @@ class BooksApp extends React.Component {
       this.setState({ searchBooks })
     })
 
-
-    let reg = /^[a-z]+$/i;
-    if(reg.test(query)) //Check letters only
+    let termies =['Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS']
+    let lower = termies.map(term => term.toLowerCase())
+    console.log(lower)
+    if (lower.indexOf(query) > -1) {
     this.setState({ query: query.trim() })
+    } else {
+    console.log("No")
+    }
+
+    //
+    // let reg = /^[a-z]+$/i;
+    // if(reg.test(query)) //Check letters only
+
     console.log(this.state)
   }
 
