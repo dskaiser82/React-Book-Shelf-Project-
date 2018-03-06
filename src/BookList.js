0202543a3ff043d7fe12b925ff8390ba01fba3eb
Dan Kaiser <dskaiser82@gmail.com>
2018-03-06 15:16:03 -0800

    slimming down unneeded html

--- src/BookList.js
+++ src/BookList.js
@@ -53,7 +53,7 @@ shelves.map(shelf =>
                   <ol className="books-grid">
 
                     {shelves.map(shelf =>
-                     shelf.theBooks
+                     shelf.theBooks.filter(book => book.shelf === "currentlyReading")
                      .map(book =>
 
                        <li key={book.id}>
@@ -88,118 +88,7 @@ shelves.map(shelf =>
         }
 
 
-              <div className="list-books-content">
-                <div>
-
-                  {/* Book Section */}
-                  <div className="bookshelf">
-                    <h2 className="bookshelf-title">Currently Reading</h2>
-                    <div className="bookshelf-books">
-                      <ol className="books-grid">
-
-                        {allBooks.filter(book => book.shelf === "currentlyReading")
-                          .map((book) =>(
-                            <li key={book.id}>
-                              <div className="book">
-                                <div className="book-top">
-                                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                                  <div className="book-shelf-changer">
-
-                                    <select id={book.id}
-                                      onChange={(event) => grabTarget(event.target.value, event.target.id)}
-                                      >
-
-                                      <option value="currentlyReading">Currently Reading</option>
-                                      <option value="wantToRead">Want to Read</option>
-                                      <option value="read">Read</option>
-                                      <option value="none">None</option>
-                                    </select>
-                                  </div>
-                                </div>
-                                <div className="book-title">{book.title}</div>
-                                <div className="book-authors">{book.authors}</div>
-                              </div>
-                            </li>
-                          ))
-                        }
-                      </ol>
-                    </div>
-                  </div>
-
-                  {/* Book Section */}
-                  <div className="bookshelf">
-                    <h2 className="bookshelf-title">Want to Read</h2>
-                    <div className="bookshelf-books">
-                      <ol className="books-grid">
-
-                        {allBooks.filter(book => book.shelf === "wantToRead")
-                          .map((book) =>(
-                            <li key={book.id}>
-                              <div className="book">
-                                <div className="book-top">
-                                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                                  <div className="book-shelf-changer">
-
-                                    <select id={book.id}
-                                      onChange={(event) => grabTarget(event.target.value, event.target.id)}
-                                      >
-
-                                      <option value="wantToRead">Want to Read</option>
-                                      <option value="currentlyReading">Currently Reading</option>
-                                      <option value="read">Read</option>
-                                      <option value="none">None</option>
-                                    </select>
-                                  </div>
-                                </div>
-                                <div className="book-title">{book.title}</div>
-                                <div className="book-authors">{book.authors}</div>
-                              </div>
-                            </li>
-                          ))
-                        }
-                      </ol>
-                    </div>
-                  </div>
-
-                  {/* Book Section */}
-                  <div className="bookshelf">
-                    <h2 className="bookshelf-title">Have Read</h2>
-                    <div className="bookshelf-books">
-                      <ol className="books-grid">
-
-                        {allBooks.filter(book => book.shelf === "read")
-                          .map((book) =>(
-                            <li key={book.id}>
-                              <div className="book">
-                                <div className="book-top">
-                                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                                  <div className="book-shelf-changer">
-
-                                    <select id={book.id}
-                                      onChange={(event) => grabTarget(event.target.value, event.target.id)}
-                                      >
-
-                                      <option value="read">Read</option>
-                                      <option value="currentlyReading">Currently Reading</option>
-                                      <option value="wantToRead">Want to Read</option>
-                                      <option value="none">None</option>
-
-                                    </select>
-                                  </div>
-                                </div>
-                                <div className="book-title">{book.title}</div>
-                                <div className="book-authors">{book.authors}</div>
-                              </div>
-                            </li>
-                          ))
-                        }
-                      </ol>
-                    </div>
-                  </div>
-
-                </div>
-              </div>
-
+            
               {/* Open Search */}
               <Link
                   to="/search"
